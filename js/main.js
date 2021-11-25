afac2a53998742c7ebcccf33fa0fc1f174a29dc8
Jesper Hagman <Jesper.hagman98@gmail.com>
2021-11-25 11:29:18 +0100

    first step

--- js/main.js
+++ js/main.js
@@ -4,7 +4,28 @@
 
 let city = document.getElementById("city")
 
-function temperature() {
-    var mylist = document.getElementById("myList");
-    document.getElementById("favourite").value = mylist.options[mylist.selectedIndex].text;
+function temperature(temp) {
+    let typOfTemprature = document.getElementById("typOfTemprature");
+    let unit = typOfTemprature.options[typOfTemprature.selectedIndex].text;
+    console.log(unit)
+
+    if (unit == "Fahrenheit") {
+        let temp = "imperial"
+    } else {
+        temp = "metric"
     }
+}
+
+
+async function fetchData(temp) {
+    try {
+        let response = await fetch(`api.openweathermap.org/data/2.5/weather?q=${city.value}&units=${temp.value}&appid=4423845cee6ed32c6e6b2e17f19bbdc9`)
+        let data = await response.json()
+
+        console.log(data)
+
+    }catch(error) {
+
+    }
+}
+
