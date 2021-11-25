6022634bb942eafe38630441e72dca944266fafc
Jesper Hagman <Jesper.hagman98@gmail.com>
2021-11-25 17:51:16 +0100

    still stuck at the split for metric and imperial

--- js/main.js
+++ js/main.js
@@ -16,15 +16,14 @@ async function fetchData() {
     try {
         let response = await fetch(`http://api.openweathermap.org/data/2.5/find?q=${city.value}&units=metric&appid=4423845cee6ed32c6e6b2e17f19bbdc9`)
         let data = await response.json()
-        console.log(data)
-
+        
         getWeatherFromData(data)
-
-        changeMeasurementType(e)
-
-      
+        
+        
+        console.log(data)
     } catch(error) {
-        contentDiv.innerHTML += "please double check your spelling. We apologize for the inconvenience"
+        /* contentDiv.innerHTML += "We apologize for the inconvenience it seems that we have some technical problems!" */
+        console.log(error)
     }
 }
 
@@ -33,25 +32,16 @@ function getWeatherFromData(data) {
         <article>
             <h2>The weather in ${data.list[0].name}:</h2>
             <p>The local forecast says it's ${Math.round(data.list[0].main.temp)}° 
-            degrees celsius outside. And it will feel like ${Math.round(data.list[0].main.feels_like)}°.</p>
+            degrees outside. And it will feel like ${Math.round(data.list[0].main.feels_like)}°.</p>
             <p>The humidity will be around ${Math.round(data.list[0].main.humidity)}%.</p>
             <p>there will be ${data.list[0].weather[0].description}</p>
         </article>
     `
 }
 
-function changeMeasurementType (e) {
-
-
-    let result = document.querySelector('#content');
-    result.innerHTML = `${e.target.value}`;
-
-    result.textContent = `${e.target.value}`;
-        if (e.target.value == "imperial") {
-            result.textContent += " Jag har löst det tror jag"
-        
-          }
-  
-  
+function changeMeasurementType (event) {
+    unit = `${event.target.value}`;
+    
+    contentDiv.textContent += `You like ${event.target.value}`;
     
 }
