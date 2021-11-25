16e9a7c3dd60f200d631a42269438d3ee9606f16
Jesper Hagman <Jesper.hagman98@gmail.com>
2021-11-25 11:00:25 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// my API key: 4423845cee6ed32c6e6b2e17f19bbdc9
+
+
+
+let city = document.getElementById("city")
+
+function temperature() {
+    var mylist = document.getElementById("myList");
+    document.getElementById("favourite").value = mylist.options[mylist.selectedIndex].text;
+    }
