cffc4f127296745f1b50e7a33292d09dadbbfb5b
Jesper Hagman <Jesper.hagman98@gmail.com>
2021-11-25 15:11:38 +0100

    done with the first part

--- js/main.js
+++ js/main.js
@@ -2,45 +2,42 @@
 
 
 
-let city = document.getElementById("city")
+let city       = document.getElementById("city")
 let contentDiv = document.getElementById('content')
+let celsius    = "°"
+let fahrenheit = "°F"
 
 city.addEventListener("change", fetchData)
 
 async function fetchData() {
     try {
-        let response = await fetch(`http://api.openweathermap.org/data/2.5/find?q=${city.value}&units=metric&appid=4423845cee6ed32c6e6b2e17f19bbdc9`);
-        if(!response.ok) {
-            throw new Error('Some network problems')
-        }
+        let response = await fetch(`http://api.openweathermap.org/data/2.5/find?q=${city.value}&units=metric&appid=4423845cee6ed32c6e6b2e17f19bbdc9`)
         let data = await response.json()
         console.log(data)
 
         getWeatherFromData(data)
-        
     } catch(error) {
-        console.log(error)
+        contentDiv.innerHTML += "please double check your spelling. We apologize for the inconvenience"
     }
 }
 
-/* function temperature(unit) {
-    let typOfTemprature = document.getElementById("typOfTemprature");
-    unit = typOfTemprature.options[typOfTemprature.selectedIndex].text;
-    console.log(unit)
-} */
-
 function getWeatherFromData(data) {
-    console.log(data.list[0].main)
-    let content = document.getElementById("content")
-    content.innerHTML = `
+    contentDiv.innerHTML += `
         <article>
             <h2>The weather in ${data.list[0].name}:</h2>
-            <p>The local forcast says it's ${Math.round(data.list[0].main.temp)}° 
-            degrees celsius outside. But it will feel like ${Math.round(data.list[0].main.feels_like)}°.</p>
-            <p>the humidity will be around ${Math.round(data.list[0].main.humidity)}%.</p>
-            <p></p>
-            <p></p>
-            
+            <p>The local forecast says it's ${Math.round(data.list[0].main.temp)}° 
+            degrees celsius outside. And it will feel like ${Math.round(data.list[0].main.feels_like)}°.</p>
+            <p>The humidity will be around ${Math.round(data.list[0].main.humidity)}%.</p>
+            <p>there will be ${data.list[0].weather[0].description}</p>
         </article>
     `
+}
+
+function checkFluency()
+{
+  var checkbox = document.getElementById('fluency');
+  if (checkbox.checked != true)
+  {
+    alert("you need to be fluent in English to apply for the job");
+  }
 }
