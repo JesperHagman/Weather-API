abc946878f89bd87c4756e071762b816010ed3dc
Jesper Hagman <Jesper.hagman98@gmail.com>
2021-11-30 15:43:17 +0100

    will try to add som img

--- js/main.js
+++ js/main.js
@@ -21,18 +21,18 @@ async function fetchData(location) {
         let data = await response.json();
         return data
     } else {
-        let response = await fetch(`http://api.openweathermap.org/data/2.5/find?q=${location}&units=metric&appid=4423845cee6ed32c6e6b2e17f19bbdc9`)
+        let response = await fetch(`http://api.openweathermap.org/data/2.5/find?q=${location}&appid=4423845cee6ed32c6e6b2e17f19bbdc9`)
         let data = await response.json();
         return data
     }
 }
-async function getWeatherFromData() {
+async function getWeatherFromData(temp) {
     let data = []
     let location = city.value
     try {
         data = await fetchData(location)
         clearText()
-        contentDiv.innerHTML += `
+        contentDiv.innerHTML = `
         <article>
         <h2>The weather in ${data.list[0].name}:</h2>
         <p>The local forecast says it's ${Math.round(data.list[0].main.temp)}° 
@@ -41,10 +41,20 @@ async function getWeatherFromData() {
         <p>There will be ${data.list[0].weather[0].description}</p>
         </article>
         `
+        temp = Math.round(data.list[0].main.temp)
     }catch(error) {
         console.log(error)
+        contentDiv.innerHTML = `<p>${location} is an invalid search! Please try again</p>`
     }
 }
 function clearText() {
     city.value = onfocus = this.value = ""
+    contentDiv.innerHTML = ""
+}
+
+function changeImg(temp){
+    if (temp <= 0) {
+
+    }
+    
 }
